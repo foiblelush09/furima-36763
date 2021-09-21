916fcc68cb852218427fb27d1ab6bddd7634df88
foiblelush09 <fcb.football.1015@gmail.com>
2021-09-21 17:04:23 +0900

    バリデーション記入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
+  validates :image,       presence: true
+  validates :title,       presence: true
+  validates :discription, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price,       presence: true, format: {with: /\A[0-9]+\z/}, numericality: {in: 300..9999999}
 end
