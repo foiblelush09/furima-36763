9285b6216c325b12fce8d2717ffbc644c405edba
foiblelush09 <fcb.football.1015@gmail.com>
2021-09-22 11:30:11 +0900

    Itemモデル単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    image {Faker::Lorem.sentence}
+    title {"タイトル"}
+    description {"説明"}
+    category_id {2}
+    condition_id {2}
+    shipping_charge_id {2}
+    prefecture_id {2}
+    days_to_ship_id {2}
+    price {1000}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/output-image2.png'), filename: 'output-image2.png')
+    end
   end
 end
