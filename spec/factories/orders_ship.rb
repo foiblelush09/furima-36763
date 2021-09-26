5a14cb6b02da40afd806edac057020329cd43b0c
foiblelush09 <fcb.football.1015@gmail.com>
2021-09-26 16:18:11 +0900

    FactoryBot生成

---
+++ spec/factories/orders_ship.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :order_ship do
+    postalcode {'111-1111'}
+    prefecture_id {2}
+    city {'山田市'}
+    address {'山田町'}
+    phonenumber {'00000000000'}
+  end
+end
