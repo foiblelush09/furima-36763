29fb983dafa6901e5bd0712b3705c600e0aa4ed9
foiblelush09 <fcb.football.1015@gmail.com>
2021-09-26 16:16:58 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to:"items#index"
   post 'items/new' => 'items#new'
-  resources :items
+
+  resources :items do
+    resources :orders, only: [:index, :create]
+  end
 end
