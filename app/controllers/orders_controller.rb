fca4cc46c299cb35ab65fbe8c640a7520fc66412
foiblelush09 <fcb.football.1015@gmail.com>
2021-09-28 18:11:26 +0900

    クレジットカード決済機能３

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,12 +2,16 @@ class OrdersController < ApplicationController
   def index
     @order_ship = OrderShip.new
     @item = Item.find(params[:item_id])
+    if @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def create
     @item = Item.find(params[:item_id])
     @order_ship = OrderShip.new(order_params)
     if @order_ship.valid?
+      pay_item
       @order_ship.save
       redirect_to root_path
     else
@@ -18,7 +22,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_ship).permit(:postalcode, :prefecture_id, :city, :address, :building_name, :phonenumber).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_ship).permit(:postalcode, :prefecture_id, :city, :address, :building_name, :phonenumber).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token], price: @item.price)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+      )
   end
   
 end
