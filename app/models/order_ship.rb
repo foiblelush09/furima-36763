8df39d509a3ed28e9ab8cb4deb40aefd0b4eeec6
foiblelush09 <fcb.football.1015@gmail.com>
2021-09-28 18:12:03 +0900

    token追加

--- app/models/order_ship.rb
+++ app/models/order_ship.rb
@@ -1,18 +1,19 @@
 class OrderShip
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postalcode, :prefecture_id, :city, :address, :building_name, :phonenumber
+  attr_accessor :user_id, :item_id, :postalcode, :prefecture_id, :city, :address, :building_name, :phonenumber, :token, :price, :order_id
 
   with_options presence: true do
     validates :postalcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city, :address, :user_id, :item_id
     validates :phonenumber, format: {with: /\A[0-9]+\z/, message: "input only number"}
+    validates :token
   end
 
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-
-    Ship.create(postalcode: postalcode, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phonenumber: phonenumber)
+    
+    Ship.create(postalcode: postalcode, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phonenumber: phonenumber, order_id: order_id)
   end
 end
