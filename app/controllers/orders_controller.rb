87df42c3920c6714d17653a1620dbfcfab66e168
foiblelush09 <fcb.football.1015@gmail.com>
2021-09-28 18:22:37 +0900

    自身で出品した商品を買えない記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,9 @@ class OrdersController < ApplicationController
     if @item.order.present?
       redirect_to root_path
     end
+    if user_signed_in?
+      redirect_to root_path
+    end
   end
 
   def create
